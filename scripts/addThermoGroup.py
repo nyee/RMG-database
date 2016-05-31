bef4c1c8c42d67d037b6c59a22a811d4816b0a04
Nathan Yee <nyee@mit.edu>
2016-05-31 18:00:51 -0400

    First shot at making actual function to add groups

--- scripts/addThermoGroup.py
+++ scripts/addThermoGroup.py
@@ -69,18 +69,18 @@ def findPlaceInTree(database, newNode):
     """
     #Check for matching node:
     identical=checkIdenticalNode(database, newNode)
-    if identical:
-        print "There is already an identical node for", newNode.label, "which is", identical
+    # if identical:
+    #     print "There is already an identical node for", newNode.label, "which is", identical
 
     #check for overlapping parents
     directParents=getAncestorsForNewNode(database, newNode, True)
 
     if len(directParents)>1:
-        print "There is more than one direct parent for", newNode.label, "which are", str(directParents)
+        # print "There is more than one direct parent for", newNode.label, "which are", str(directParents)
         return (None, identical, [])
     else:
         parent=directParents[0]
-        print "There is one parent for", newNode.label, "which is", parent.label
+        # print "There is one parent for", newNode.label, "which is", parent.label
 
     #Check direct children have "parent" as parent
     directChildren=getDescendentsForNewNode(database, newNode, True)
@@ -90,6 +90,66 @@ def findPlaceInTree(database, newNode):
                 "but not a child of its parent", parent.label
 
     return (parent, identical, directChildren)
+
+def addThermoGroup(database, newNode):
+    """
+    Returns a new database object with newNode added correctly into the database
+
+    treeInfo is the tuple returned from findPlaceInTree
+    """
+    #make a deep copy to scan through: Because of the isomorphism checks, we scramble the adjLists
+    #so we scan through the copy, but actually edit the original
+    print newNode.label
+    databaseCopy=copy.deepcopy(database)
+    treeInfo=findPlaceInTree(databaseCopy, newNode)
+
+    #define variables for the relatives in the original database
+    (parentCopy, identicalCopy, directChildrenCopy)=treeInfo
+    parent=database.entries[parentCopy.label]
+    if identicalCopy:
+        identical=database.entries[identicalCopy.label]
+    directChildren=[database.entries[child.label] for child in directChildrenCopy]
+
+    if not identicalCopy is None:
+        #if identical has pointer for Thermo, copy data and all metaData
+        if isinstance(identical.data, basestring):
+            print identical, "'s data was replaced"
+            identical.data=newNode.data
+            identical.reference = newNode.reference
+            identical._longDesc = newNode._longDesc
+            identical._shortDesc = newNode._shortDesc
+            identical._longDesc = newNode.longDesc
+            identical._shortDesc = newNode.shortDesc
+            identical.rank = newNode.rank
+            identical.referenceType = newNode.referenceType
+        else:
+            print identical, "and", newNode, "are identical and both have ThermoObjects."
+            #write code for user to decide which one to use
+        pass
+    else:
+        #This is the case where the new node is completely new
+        print "Adding", newNode
+        database.entries[newNode.label]=newNode
+        #check this where does it go, end?
+        parent.children.append(newNode)
+        newNode.children=directChildren
+
+        #remove children from parent
+        for child in directChildren:
+            parent.children.remove(child)
+
+        #check data of children
+        for child in directChildren:
+            if isinstance(child.data, basestring):
+                targetName=child.data.strip()
+                target=database.entries[targetName]
+                if database.getTreeDepth(target) < database.getTreeDepth(newNode):
+                    print "Changed thermo data for", child
+                    child.data=unicode(newNode.label)
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -106,13 +166,17 @@ if __name__ == "__main__":
 
     specificThermoDatabase = database.thermo.groups[groupName]
 
-    test1=specificThermoDatabase.entries["C"].data
-    # print test1
-    # testGroup=newGroups.entries['Cs-CsCsCsOs']
-    # print getAncestorsForNewNode(specificThermoDatabase, testGroup)
-    # print getDescendentsForNewNode(specificThermoDatabase, testGroup)
-    # print checkIdenticalNode(specificThermoDatabase, testGroup)
+    for entryName, entry in newGroups.entries.iteritems():
+        addThermoGroup(specificThermoDatabase, entry)
 
-    for name, entry in newGroups.entries.iteritems():
-        print name
-        print findPlaceInTree(specificThermoDatabase, entry)
+
+    # test1=specificThermoDatabase.entries["C"].data
+    # # print test1
+    # # testGroup=newGroups.entries['Cs-CsCsCsOs']
+    # # print getAncestorsForNewNode(specificThermoDatabase, testGroup)
+    # # print getDescendentsForNewNode(specificThermoDatabase, testGroup)
+    # # print checkIdenticalNode(specificThermoDatabase, testGroup)
+    #
+    # for name, entry in newGroups.entries.iteritems():
+    #     print name
+    #     print findPlaceInTree(specificThermoDatabase, entry)
