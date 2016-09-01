89485c7dfdd2f94f4c892c708c1f26d379db8757
Nathan Yee <nyee@mit.edu>
2016-09-01 01:09:15 -0400

    New H-Abstraction training reactions added for isobutanol

--- input/kinetics/families/H_Abstraction/training/reactions.py
+++ input/kinetics/families/H_Abstraction/training/reactions.py
@@ -1503,11 +1503,410 @@ entry(
         Tmin = (700, 'K'),
         Tmax = (2000, 'K'),
     ),
+    rank = 2, 
     shortDesc = u"""Zador CCSD(T) calc""",
     longDesc = 
 u"""
 Rate comes from quantum calculation by J. Zador at CCSD(T) level
 [ This rate was obtained by personal communication as of Sept 2012]
+""",
+)
+
+entry(
+    index = 1012,
+    label = "C4H10O-10 + OH <=> H2O + C4H9O-10",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.54, 'cm^3/(mol*s)'), n=3.7, Ea=(-4.94, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 4,
+    shortDesc = u"""Evans Polyani like correlation on beta H-abstraction from n-butanol""",
+    longDesc = 
+u"""
+Taken from entry: Evans-Polyani type correlations E_iso = E_n + 0.4 (BDE_n - BdE_iso)to get
+energy barrier using bond dissociation energies. CalculatedS. Merchant.
+Original rate parameters for n-butanol from Zhou et aland calculated with
+VRC-TST at CCSD(T)/cc-pVQZ/MP2/6-311G(d,p)
+""",
+)
+
+entry(
+    index = 1013,
+    label = "C4H10O-12 + OH <=> H2O + C4H9O-12",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(5.4e+06, 'cm^3/(mol*s)'), n=2, Ea=(0.51, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Eric S. Kwok", "Roger Atkinson"],
+        title = u'Estimation of Hydroxyl Radical Reaction Rate Constants for Gas-Phase Organic Compounds using a Structure-Reactivity Relationship: An Update',
+        journal = "Atmos. Environ.",
+        volume = "29",
+        pages = """1685-1695""",
+        year = "1995",
+    ),
+    referenceType = "Article",
+    rank = 4,
+    shortDesc = u"""Structure-reactivity approach proposed by Atkins for OH hydrogen abstraction from organic compounds""",
+    longDesc = 
+u"""
+Taken from entry:
+""",
+)
+
+entry(
+    index = 1015,
+    label = "C4H10O-11 + H <=> H2 + C4H9O-11",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (1.09e+07, 'cm^3/(mol*s)'),
+        n = 1.59,
+        Ea = (3.35, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1016,
+    label = "C4H10O-10 + H <=> H2 + C4H9O-10",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (1.74e+07, 'cm^3/(mol*s)'),
+        n = 1.48,
+        Ea = (3.44, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1017,
+    label = "C4H10O-12 + H <=> H2 + C4H9O-12",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (2.7e+07, 'cm^3/(mol*s)'),
+        n = 1.76,
+        Ea = (7.45, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1018,
+    label = "C4H10O-13 + H <=> H2 + C4H9O-13",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(40500, 'cm^3/(mol*s)'), n=2.38, Ea=(9.34, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1019,
+    label = "C4H10O-11 + CH3 <=> CH4 + C4H9O-11",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.87, 'cm^3/(mol*s)'), n=3.5, Ea=(6, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1020,
+    label = "C4H10O-10 + CH3 <=> CH4 + C4H9O-10",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(414, 'cm^3/(mol*s)'), n=2.87, Ea=(4.89, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1021,
+    label = "C4H10O-12 + CH3 <=> CH4 + C4H9O-12",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.61, 'cm^3/(mol*s)'), n=3.59, Ea=(7.7, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1022,
+    label = "C4H10O-13 + CH3 <=> CH4 + C4H9O-13",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(2.32, 'cm^3/(mol*s)'), n=3.49, Ea=(6, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1023,
+    label = "C4H10O-11 + C2H3 <=> C2H4 + C4H9O-11",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(0.0519, 'cm^3/(mol*s)'), n=3.9, Ea=(0.86, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1024,
+    label = "C4H10O-10 + C2H3 <=> C2H4 + C4H9O-10",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(0.0269, 'cm^3/(mol*s)'), n=3.9, Ea=(0.68, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1025,
+    label = "C4H10O-12 + C2H3 <=> C2H4 + C4H9O-12",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(0.0011, 'cm^3/(mol*s)'), n=4.55, Ea=(3.5, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1026,
+    label = "H2O2 + C4H9O-11 <=> C4H10O-11 + HO2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(56.9, 'cm^3/(mol*s)'), n=3.04, Ea=(1.2, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1027,
+    label = "H2O2 + C4H9O-10 <=> C4H10O-10 + HO2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(2.91, 'cm^3/(mol*s)'), n=3.31, Ea=(1.4, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
+""",
+)
+
+entry(
+    index = 1028,
+    label = "H2O2 + C4H9O-12 <=> C4H10O-12 + HO2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(3.01, 'cm^3/(mol*s)'), n=3.28, Ea=(1, 'kcal/mol'), T0=(1, 'K')),
+    reference = Article(
+        authors = ["Shamel S. Merchant", "Everton Fernando Zanoelo", "Raymond L. Speth", "Michael R. Harper", "Kevin M. Van Geem", "William H. Green"],
+        title = u'Combustion and pyrolysis of iso-butanol: Experimental and chemical kinetic modeling study',
+        journal = "Combust. Flame.",
+        volume = "160",
+        pages = """1907-1929""",
+        year = "2013",
+    ),
+    referenceType = "Article",
+    rank = 3,
+    shortDesc = u"""CBS-QB3 calculation with 1-d rotor treatment at B3LYP/631G(d)""",
+    longDesc = 
+u"""
+Taken from entry: Quantum chemistry calculations CBS-QB3 calculation with 1-d rotor treatment at
+B3LYP/631G(d)" using Gaussian 03 and Gaussian 09. High-pressure-limit rate
+coefficient computed TST with Eckart Tunnelling"
 """,
 )
 
