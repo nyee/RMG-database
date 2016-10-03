1751e649e860c62caca3b47dddf9a3e1f60421e9
Nathan Yee <nyee@mit.edu>
2016-10-03 17:56:53 -0400

    Update calcultaed rates with error corrections
    Also changed some reaction barriers (recalculated with cantherm) for H2O elimination on the beta and gamme surface based on comparisons with Welz. Finally added Waddington step 2 to library.

--- input/kinetics/libraries/isobutanol/reactions.py
+++ input/kinetics/libraries/isobutanol/reactions.py
@@ -5,38 +5,38 @@ name = 'iButanol_peroxy'
 shortDesc = u'Peroxy chemistry for isobutanol'
 longDesc = ''
 
-# entry(
-#     index = 1,
-#     label = 'Ap11 <=> Ap07 + HO2',
-#     degeneracy = 1,
-#     kinetics = Arrhenius(
-#         A = (6.443e+12, 's^-1'),
-#         n = -0.016,
-#         Ea = (12.431, 'kcal/mol'),
-#         T0 = (1, 'K'),
-#     ),
-# )
+entry(
+    index = 1,
+    label = 'Ap01 + OH <=> Ap10',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (1.807e+02, 'cm^3/(mol*s)'),
+        n = 3.053,
+        Ea = (22.492, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
 
 entry(
     index = 2,
-    label = 'Ap07 + HO2 <=> Ap11',
+    label = 'Ap02 + H <=> Ap13',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (1.764e-01, 'cm^3/(mol*s)'),
-        n = 3.274,
-        Ea = (-4.883, 'kcal/mol'),
+        A = (1.066e+05, 'cm^3/(mol*s)'),
+        n = 2.559,
+        Ea = (6.908, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 3,
-    label = 'Ap11 <=> Ap12',
+    label = 'Ap03 + OH <=> Ap12',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (1.756e+03, 's^-1'),
-        n = 2.326,
-        Ea = (20.469, 'kcal/mol'),
+        A = (4.039e+01, 'cm^3/(mol*s)'),
+        n = 3.233,
+        Ea = (33.314, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
@@ -55,6 +55,18 @@ entry(
 
 entry(
     index = 5,
+    label = 'Ap06 + CH3 <=> Ap12',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (9.145e+02, 'cm^3/(mol*s)'),
+        n = 2.495,
+        Ea = (6.841, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
+
+entry(
+    index = 6,
     label = 'Ap07 + HO2 <=> Ap13',
     degeneracy = 1,
     kinetics = Arrhenius(
@@ -64,21 +76,37 @@ entry(
         T0 = (1, 'K'),
     ),
 )
-
+#use two irreversible reactions to handle submerged TS
 entry(
-    index = 6,
-    label = 'Ap03 + OH <=> Ap12',
+    index = 7,
+    label = 'Ap11 => Ap07 + HO2',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (4.039e+01, 'cm^3/(mol*s)'),
-        n = 3.233,
-        Ea = (33.314, 'kcal/mol'),
+        A = (6.74952e+11, 's^-1'),
+        n = 0.258677,
+        Ea = (11.598, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
+    reversible = False,
+    duplicate = True,
 )
 
 entry(
-    index = 7,
+    index = 8,
+    label = 'Ap07 + HO2 => Ap11',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (5.24825e+15, 'cm^3/(mol*s)'),
+        n = -3.92085,
+        Ea = (5.113, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    reversible = False,
+    duplicate = True,
+)
+
+entry(
+    index = 9,
     label = 'Ap09 + HO2 <=> Ap11',
     degeneracy = 1,
     kinetics = Arrhenius(
@@ -90,128 +118,68 @@ entry(
 )
 
 entry(
-    index = 8,
+    index = 10,
     label = 'Ap09 + HO2 <=> Ap10',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (1.863e-02, 'cm^3/(mol*s)'),
-        n = 4.028,
-        Ea = (0.781, 'kcal/mol'),
-        T0 = (1, 'K'),
-    ),
-)
-
-entry(
-    index = 9,
-    label = 'Ap02_bmk + H_bmk <=> Ap12',
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (9.754e+04, 'cm^3/(mol*s)'),
-        n = 2.570,
-        Ea = (6.891, 'kcal/mol'),
-        T0 = (1, 'K'),
-    ),
-)
-
-entry(
-    index = 10,
-    label = 'Ap10 <=> Ap11',
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (4.583e-04, 's^-1'),
-        n = 4.110,
-        Ea = (10.101, 'kcal/mol'),
+        A = (1.854e-02, 'cm^3/(mol*s)'),
+        n = 4.029,
+        Ea = (1.261, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 11,
-    label = 'Ap10 <=> Ap13',
+    label = 'Ap10 <=> Ap11',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (4.197e-07, 's^-1'),
-        n = 5.119,
-        Ea = (23.116, 'kcal/mol'),
+        A = (1.873e-04, 's^-1'),
+        n = 4.222,
+        Ea = (10.634, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 12,
-    label = 'Ap12 <=> Ap13',
+    label = 'Ap10 <=> Ap13',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (1.524e+02, 's^-1'),
-        n = 2.659,
-        Ea = (14.135, 'kcal/mol'),
+        A = (3.402e-07, 's^-1'),
+        n = 5.146,
+        Ea = (23.407, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 13,
-    label = 'Ap06_bmk + CH3_bmk <=> Ap12',
+    label = 'Ap11 <=> Ap12',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (9.145e+02, 'cm^3/(mol*s)'),
-        n = 2.495,
-        Ea = (6.841, 'kcal/mol'),
+        A = (1.756e+03, 's^-1'),
+        n = 2.326,
+        Ea = (20.469, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 14,
-    label = 'Bp3 + HO2 <=> Bp9',
+    label = 'Ap12 <=> Ap13',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (4.254e-06, 'cm^3/(mol*s)'),
-        n = 4.866,
-        Ea = (4.213, 'kcal/mol'),
+        A = (4.950e+01, 's^-1'),
+        n = 2.800,
+        Ea = (13.864, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 15,
-    label = 'Bp3 + HO2 <=> Bp7',
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (1.176e-06, 'cm^3/(mol*s)'),
-        n = 4.765,
-        Ea = (0.880, 'kcal/mol'),
-        T0 = (1, 'K'),
-    ),
-)
-
-entry(
-    index = 16,
-    label = 'Ap09 + HO2 <=> Bp6',
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (1.459e+00, 'cm^3/(mol*s)'),
-        n = 3.140,
-        Ea = (1.462, 'kcal/mol'),
-        T0 = (1, 'K'),
-    ),
-)
-
-entry(
-    index = 17,
-    label = 'Ap09 + HO2 <=> Bp7',
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (1.119e-01, 'cm^3/(mol*s)'),
-        n = 3.916,
-        Ea = (8.708, 'kcal/mol'),
-        T0 = (1, 'K'),
-    ),
-)
-
-entry(
-    index = 18,
-    label = 'Bp1 + OH <=> Bp6',
+    label = 'Ap01 + OH <=> Bp6',
     degeneracy = 1,
     kinetics = Arrhenius(
         A = (2.699e+03, 'cm^3/(mol*s)'),
@@ -222,55 +190,7 @@ entry(
 )
 
 entry(
-    index = 19,
-    label = 'Bp2 + OH <=> Bp9',
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (1.117e+03, 'cm^3/(mol*s)'),
-        n = 2.738,
-        Ea = (26.445, 'kcal/mol'),
-        T0 = (1, 'K'),
-    ),
-)
-
-entry(
-    index = 20,
-    label = 'Bp6 <=> Bp11 + H2O',
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (1.124e+09, 's^-1'),
-        n = 1.035,
-        Ea = (21.597, 'kcal/mol'),
-        T0 = (1, 'K'),
-    ),
-)
-
-entry(
-    index = 21,
-    label = 'Bp6 <=> Bp7',
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (2.276e-03, 's^-1'),
-        n = 4.176,
-        Ea = (9.947, 'kcal/mol'),
-        T0 = (1, 'K'),
-    ),
-)
-
-entry(
-    index = 22,
-    label = 'Bp7 <=> Bp8',
-    degeneracy = 1,
-    kinetics = Arrhenius(
-        A = (1.448e+13, 's^-1'),
-        n = -0.487,
-        Ea = (21.085, 'kcal/mol'),
-        T0 = (1, 'K'),
-    ),
-)
-
-entry(
-    index = 23,
+    index = 16,
     label = 'Bp7 <=> Bp9',
     degeneracy = 1,
     kinetics = Arrhenius(
@@ -282,49 +202,145 @@ entry(
 )
 
 entry(
-    index = 24,
-    label = 'Gp09 <=> Gp11',
+    index = 17,
+    label = 'Bp6 <=> Bp11 + H2O',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (4.942e-06, 's^-1'),
-        n = 4.976,
-        Ea = (18.644, 'kcal/mol'),
+        A = (9.248e+08, 's^-1'),
+        n = 1.059,
+        Ea = (7.914, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
+
+entry(
+    index = 18,
+    label = 'Bp2 + OH <=> Bp9',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (1.117e+03, 'cm^3/(mol*s)'),
+        n = 2.738,
+        Ea = (26.445, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
+
+entry(
+    index = 19,
+    label = 'Bp3 + HO2 <=> Bp9',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (4.254e-06, 'cm^3/(mol*s)'),
+        n = 4.866,
+        Ea = (4.213, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
+
+entry(
+    index = 20,
+    label = 'Bp3 + HO2 <=> Bp7',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (1.176e-06, 'cm^3/(mol*s)'),
+        n = 4.765,
+        Ea = (0.880, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
+
+entry(
+    index = 21,
+    label = 'Ap09 + HO2 <=> Bp7',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (1.119e-01, 'cm^3/(mol*s)'),
+        n = 3.916,
+        Ea = (8.708, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
+
+entry(
+    index = 22,
+    label = 'Ap09 + HO2 <=> Bp6',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (1.459e+00, 'cm^3/(mol*s)'),
+        n = 3.140,
+        Ea = (1.462, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
+
+entry(
+    index = 23,
+    label = 'Bp6 <=> Bp7',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (2.276e-03, 's^-1'),
+        n = 4.176,
+        Ea = (9.947, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
+#changed to f12-vtz with no scaling, since with scaling does not converge
+entry(
+    index = 24,
+    label = 'Bp7 <=> Bp8',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (9.040e+12, 's^-1'),
+        n = -0.441,
+        Ea = (2.193e+01, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
+
+entry(
+    index = 25,
+    label = 'Bp8 <=> acetone + OH + CH2O',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (3.859e+10, 's^-1'),
+        n = 0.909,
+        Ea = (3.772, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 26,
-    label = 'Gp09 <=> Gp10',
+    label = 'Gp08 <=> Gp01 + H2O',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (4.602e+04, 's^-1'),
-        n = 2.057,
-        Ea = (20.011, 'kcal/mol'),
+        A = (4.283e+15, 's^-1'),
+        n = -1.207,
+        Ea = (10.028, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 27,
-    label = 'Gp09 <=> Gp08',
+    label = 'Ap03 + OH <=> Gp08',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (1.011e+02, 's^-1'),
-        n = 2.622,
-        Ea = (14.188, 'kcal/mol'),
+        A = (4.055e+04, 'cm^3/(mol*s)'),
+        n = 2.443,
+        Ea = (38.116, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 28,
-    label = 'Gp09 <=> Gp14',
+    label = 'Gp03 + OH <=> Gp10',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (4.170e+07, 's^-1'),
-        n = 0.821,
-        Ea = (21.373, 'kcal/mol'),
+        A = (4.587e-02, 'cm^3/(mol*s)'),
+        n = 4.118,
+        Ea = (33.183, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
@@ -367,42 +383,54 @@ entry(
 
 entry(
     index = 32,
-    label = 'Ap03 + OH <=> Gp08',
+    label = 'Gp09 <=> Gp08',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (4.055e+04, 'cm^3/(mol*s)'),
-        n = 2.443,
-        Ea = (38.116, 'kcal/mol'),
+        A = (1.011e+02, 's^-1'),
+        n = 2.622,
+        Ea = (14.188, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 33,
-    label = 'Gp08 <=> Gp01 + H2O',
+    label = 'Gp09 <=> Gp10',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (3.774e+15, 's^-1'),
-        n = -1.191,
-        Ea = (12.3, 'kcal/mol'),
+        A = (4.602e+04, 's^-1'),
+        n = 2.057,
+        Ea = (20.011, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
     index = 34,
-    label = 'Gp03 + OH <=> Gp10',
+    label = 'Gp09 <=> Gp11',
     degeneracy = 1,
     kinetics = Arrhenius(
-        A = (4.587e-02, 'cm^3/(mol*s)'),
-        n = 4.118,
-        Ea = (33.183, 'kcal/mol'),
+        A = (4.942e-06, 's^-1'),
+        n = 4.976,
+        Ea = (18.644, 'kcal/mol'),
         T0 = (1, 'K'),
     ),
 )
 
 entry(
-    index = 35,
+    index = 36,
+    label = 'Gp09 <=> Gp14',
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (4.170e+07, 's^-1'),
+        n = 0.821,
+        Ea = (21.373, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+)
+
+entry(
+    index = 37,
     label = 'Gp08 <=> Gp15 + OH + CH2O',
     degeneracy = 1,
     kinetics = Arrhenius(
